41957a66c29e8bfea5a64eba872c88aecc3c73d4
zhangzhenyude123123 <60837761+zhangzhenyude123123@users.noreply.github.com>
2021-06-01 21:01:45 +0800

    This version add header and footer and add min()function to realize the auto compiler

---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>@yield('title', 'LaraBBS') Laravel11111</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<div id="app" class="{{ route_class() }}-page">
+
+    @include('Layouts.header')
+
+    <div class="container">
+
+        @include('Tools.message')
+
+        @yield('content')
+
+    </div>
+
+    @include('Layouts.footer')
+</div>
+
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/Layouts/footer.blade.php
@@ -0,0 +1,6 @@
+
+<footer class="footer">
+  <div class="container">
+    <p class="float-right">版权所有 @开发小组</p>
+  </div>
+</footer>
